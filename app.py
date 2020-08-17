5f9345254ef2224fcec1926656c4ad2d8a7d1c32
Ruchin Dhama <ruchind159@gmail.com>
2020-08-18 00:22:03 +0530

    added part of speech file and cleaned output a bit

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import jsonify
 from main_file import load_model
 from main_file import predict_for_sentence
 from main_file import convert_to_dict
+from main_file import add_tokens
 
 
 app = Flask(__name__)             # create an app instance
@@ -14,37 +15,13 @@ model=load_model()				  #loading model from main_file
 def hello():                      # call method hello
     return "Hello World!"         # which returns "hello world"
 
-# @app.route("/<name>")
-# def greet(name):
-# 	return "hello "+name
 
-@app.route("/<sentence>")              
+
+@app.route("/<sentence>")              #this is same as the grammar check function in main file
 def correct_sent(sentence):
 	corrected_sent,cnt_corrections,info_for_change = predict_for_sentence(sentence, model)
 
-	org_sent_word=[]                   #changed here
-	org_sent_word=sentence.split()  #changed here
-
-	word_info=[]
-
-	for info in info_for_change:
-	    word=[info[0],info[1]]
-	    if "APPEND" in word[0]:
-	        if word[1]-1 > -1 :
-	            word[1]=org_sent_word[word[1]-1]
-	            word[0]+="_after"
-	        else:
-	            word[1]="<BEG>"
-	            word[0]+="_after"
-	            continue
-	    elif "REPLACE" in word[0]:
-	        word[1]=org_sent_word[word[1]]
-	        word[0]+=" with "
-	    else:
-	        word[1]=org_sent_word[word[1]]
-	        word[0]+=" on "
-
-	    word_info.append(word)
+	word_info=add_tokens(sentence,info_for_change)
 
 	info_dict=convert_to_dict(word_info,corrected_sent)
 	return jsonify(info_dict)
